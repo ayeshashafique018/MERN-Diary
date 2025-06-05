e6cab88de45a7e62f3d2b33fd7bc7db03c4cdd3f
ayeshashafique018 <ayeshashafique018@gmail.com>
2025-06-05 23:57:16 +0500

    express starter

---
+++ 9/test-server/index.js
@@ -0,0 +1,20 @@
+// const express = require('express');
+// const app = express();
+// const port = 3000;
+
+// app.get('/', (req, res) => {
+//   res.send('Home');
+// });
+
+
+// app.get('/blog/:slug', (req, res) => {
+//   res.send('Hello $(req.params.slug)');
+// });
+
+// app.listen(port, () => {
+//   console.log(`Listening on port ${port}`);
+// });
+// app.use((req, res, next) => {
+//   console.log(`Request URL: ${req.url}`);
+//   res.status(404).send('Sorry, that route does not exist!');
+// });
